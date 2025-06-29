bc57269ee72043c5ceb8160d5a4a825741848794
doctored11 <edit.shultz@yandex.ru>
2025-06-29 21:54:37 +0300

    l2

--- scenes/lesson02/scene.js
+++ scenes/lesson02/scene.js
@@ -1,24 +1,53 @@
-import * as THREE from 'three';
-
-//стартовый шаблон 🤔💩
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  10000
+);
+
+camera.position.set(-200, 200, 800);
+
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshNormalMaterial();
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+
+
+const ambient = new THREE.AmbientLight(0xffffff, 5);
+scene.add(ambient);
+
+const loader = new GLTFLoader();
+
+loader.load(
+  "/models/l2-car/scene.gltf",
+  function (gltf) {
+    const model = gltf.scene;
+
+    scene.add(model);
+
+    
+    // const controls = new OrbitControls(camera, renderer.domElement);
+    // controls.target.set(0, 0, 0);
+    // controls.update();
+  },
+  undefined,
+  function (error) {
+    console.error("Ошибка при загрузке:", error);
+  }
+);
+
+scene.add(new THREE.GridHelper(10, 10));
+scene.add(new THREE.AxesHelper(5));
 
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+
   renderer.render(scene, camera);
 }
 animate();
---
+++ scenes/lesson03/scene.js
@@ -0,0 +1,25 @@
+import * as THREE from 'three';
+
+//стартовый шаблон 🤔💩
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshNormalMaterial();
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+animate();
+//
